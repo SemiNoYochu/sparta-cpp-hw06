447884fa2b6d474defe140a6450b5b5b4de42f69
Semi <bpwvdf7g55@privaterelay.appleid.com>
2026-04-14 17:00:57 +0900

    [조현민][도전과제] 타이머를 이용한 일정 주기마다 나타나는 플랫폼 생성

---
+++ Source/SpartaCpp06/Private/Platform.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpartaCpp06/Public/Platform.h"
+
+
+// Sets default values
+APlatform::APlatform()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	
+	Root = CreateDefaultSubobject<USceneComponent>("Scene");
+	SetRootComponent(Root);
+	
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>("StaticMesh");
+	StaticMesh->SetupAttachment(Root);
+	
+	bIsActive = true;
+	ToggleInterval = 2.0f;
+}
+
+// Called when the game starts or when spawned
+void APlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	GetWorld()->GetTimerManager().SetTimer(
+		ToggleTimerHandle,
+		this,
+		&APlatform::TogglePlatform,
+		ToggleInterval,
+		true,
+		StartDelay
+		);
+}
+
+void APlatform::TogglePlatform()
+{
+	bIsActive = !bIsActive;
+	
+	SetActorHiddenInGame(bIsActive);
+	SetActorEnableCollision(bIsActive); 
+}
+
---
+++ Source/SpartaCpp06/Public/Platform.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Platform.generated.h"
+
+UCLASS()
+class SPARTACPP06_API APlatform : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	APlatform();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component")
+	USceneComponent* Root;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component")
+	UStaticMeshComponent* StaticMesh;
+	
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float StartDelay;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float ToggleInterval;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	bool bIsActive;
+	
+	FTimerHandle ToggleTimerHandle;
+	
+	void TogglePlatform();
+};
